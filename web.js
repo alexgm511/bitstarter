d22e4063dd4ed1d155c2138267a19d137fdf0289
alexgm511 <alexgm511@gmail.com>
2013-07-05 21:04:38 -0400

    Fixed errors 5

--- web.js
+++ web.js
@@ -2,16 +2,18 @@ var express = require('express');
 
 var fs = require('fs');
 var file = "index.html";
+var msg = '';
 
 fs.readFile(file, function (err, data) {
     if (err) throw err;
     var buf = new Buffer(data);
+    msg = buf.toString('uft-8',0,buf.length);
 });
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString('uft-8',0,buf.length));
+  response.send(buf.toString(msg));
 });
 
 var port = process.env.PORT || 5000;
