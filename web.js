2e6d4a3b061f2cefd24123ee65f6746e113e5240
alexgm511 <alexgm511@gmail.com>
2013-07-05 20:17:48 -0400

    Changed to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
+var fs = require('fs');
+var file = "index.html";
+var buf = new Buffer('hello');
+
+fs.readFile(file, function (err, data) {
+    if (err) throw err;
+    var buf = new Buffer(data);
+}
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString('uft-8',0,buf.length);
 });
 
 var port = process.env.PORT || 5000;
