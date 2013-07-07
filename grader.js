40f733f164214789ac807542d0e5796924235bbe
alexgm511 <alexgm511@gmail.com>
2013-07-07 16:51:07 -0400

    Added URL input option with use of Restler to grader.js

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler'); 
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URLFILE_DEFAULT = "index.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -34,6 +36,18 @@ var assertFileExists = function(infile) {
 	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var assertUrlExists = function(fileurl) {
+    var resp = ''; 
+    rest.get(fileurl).on('complete', function(result, response) {
+        if (result instanceof Error) {
+            console.log('Error retrieving file: ' + fileurl);
+        } else {
+	    resp = response.toString();
+	}
+	return resp;
+    });
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -65,6 +79,8 @@ if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_file>', 'URL to html file', clone(assertUrlExists), URLFILE_DEFAULT)
+
 	.parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
